cac9c5f44fc4ec7258d4944d7c51e36ddf0cc6df
evgsbel <evgsbel@gmail.com>
2023-08-14 03:09:37 +0400

    finish main without js

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,9 +1,11 @@
 "use strict";
 
 // //masked inputs
-// $(() => {
-//   Inputmask({"mask": "+7 (999) 999 - 99 - 99"}).mask('.phone-mask');
-// });
+$(function () {
+  Inputmask({
+    "mask": "+7 999 999-99-99"
+  }).mask('.phone-mask');
+});
 //
 // // tabs
 // document.addEventListener('DOMContentLoaded', function () {
@@ -48,4 +50,115 @@
 
 $('.js-open-header-catalog').click(function () {
   $(this).toggleClass('is-active');
+});
+$('.nav-tabs a').on('click', function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+});
+$(function () {
+  // select2
+  $('select').select2({
+    minimumResultsForSearch: Infinity,
+    width: '100%',
+    closeOnSelect: false
+    //custom scroll
+  }).on("select2:open", function () {
+    dropdownParent: $(this).parent();
+    var optionsContainer = $('.select2-results__options').get(0);
+    setTimeout(function () {
+      new SimpleBar(optionsContainer, {
+        autoHide: false
+      });
+    }, 0);
+  }).on('change', function () {
+    var selected = $(this).val().length;
+    var of = $(this).find('option').length;
+    if (selected > 1) $(this).parent().find('.select2 .select2-selection ul').html('<li class="select2-selection__choice">Выбрано: ' + selected + '</li>');
+  });
+});
+
+//gallery show more
+$(function () {
+  $('.js-gallery-show-more').click(function () {
+    $(this).toggleClass('active');
+    if ($(this).find('span').text() != "Свернуть") {
+      $(this).find('span').text('Свернуть');
+    } else {
+      $(this).find('span').text('Показать еще');
+    }
+  });
+});
+
+//toggle favorite and matches
+$(function () {
+  $('.js-item-card-toggle').on('click', function (e) {
+    e.preventDefault();
+    if ($(this).hasClass('item-favorites__actions-button')) {
+      $(this).parents('.item-actions__button-wrapper').toggleClass('is-active');
+    }
+    $(this).toggleClass('is-active');
+  });
+  $('.js-item-togglable-card-toggle').on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('is-active');
+  });
+});
+
+//sliders
+$(function () {
+  var homeBestSlider = new Swiper(".js-best-slider", {
+    speed: 700,
+    slidesPerView: 4,
+    watchSlidesProgress: true,
+    spaceBetween: 30
+    // breakpoints: {
+    //   0: {
+    //     pagination: {
+    //       el: ".js-home-best-slider-pagination",
+    //       clickable: true,
+    //     },
+    //     slidesPerView: 1,
+    //   },
+    //   576: {
+    //     pagination: {
+    //       el: ".js-home-best-slider-pagination",
+    //       clickable: true,
+    //     },
+    //     slidesPerView: 2,
+    //   },
+    //   768: {
+    //     pagination: {
+    //       el: ".js-home-best-slider-pagination",
+    //       clickable: true,
+    //     },
+    //     slidesPerView: 3,
+    //
+    //   },
+    //   1290: {
+    //     slidesPerView: 4,
+    //     navigation: {
+    //       nextEl: ".js-home-best-slider-next",
+    //       prevEl: ".js-home-best-slider-prev",
+    //     },
+    //   },
+    //   1560: {
+    //     slidesPerView: 4,
+    //     navigation: {
+    //       nextEl: ".js-home-best-slider-next",
+    //       prevEl: ".js-home-best-slider-prev",
+    //     },
+    //   },
+    // }
+  });
+
+  var homeNewsSlider = new Swiper(".js-news-slider", {
+    speed: 700,
+    slidesPerView: 3,
+    watchSlidesProgress: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
 });
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,7 @@
 // //masked inputs
-// $(() => {
-//   Inputmask({"mask": "+7 (999) 999 - 99 - 99"}).mask('.phone-mask');
-// });
+$(() => {
+  Inputmask({"mask": "+7 999 999-99-99"}).mask('.phone-mask');
+});
 //
 // // tabs
 // document.addEventListener('DOMContentLoaded', function () {
@@ -47,3 +47,124 @@
 $('.js-open-header-catalog').click(function () {
   $(this).toggleClass('is-active')
 })
+
+$('.nav-tabs a').on('click', function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+
+$(() => {
+
+// select2
+  $('select').select2({
+    minimumResultsForSearch: Infinity,
+    width: '100%',
+    closeOnSelect: false,
+//custom scroll
+  })
+    .on("select2:open", function () {
+      dropdownParent: $(this).parent()
+      var optionsContainer = $('.select2-results__options').get(0);
+
+      setTimeout(function () {
+        new SimpleBar(optionsContainer, {
+          autoHide: false
+        });
+      }, 0);
+
+     })
+    .on('change', function() {
+    var selected = $(this).val().length;
+    var of = $(this).find('option').length;
+
+    if (selected > 1)
+      $(this).parent().find('.select2 .select2-selection ul').html('<li class="select2-selection__choice">Выбрано: ' + selected + '</li>')
+  });
+});
+
+//gallery show more
+$(() => {
+  $('.js-gallery-show-more').click(function(){
+
+    $(this).toggleClass('active');
+    if($(this).find('span').text()!="Свернуть"){
+      $(this).find('span').text('Свернуть');
+    }else{
+      $(this).find('span').text('Показать еще');
+    }
+  });
+});
+
+//toggle favorite and matches
+$(() => {
+  $('.js-item-card-toggle').on('click', function (e) {
+    e.preventDefault();
+    if ($(this).hasClass('item-favorites__actions-button')) {
+      $(this).parents('.item-actions__button-wrapper').toggleClass('is-active');
+    }
+    $(this).toggleClass('is-active');
+  });
+  $('.js-item-togglable-card-toggle').on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('is-active');
+  });
+});
+
+//sliders
+$(() => {
+  const homeBestSlider = new Swiper(".js-best-slider", {
+    speed: 700,
+    slidesPerView: 4,
+    watchSlidesProgress: true,
+    spaceBetween: 30,
+    // breakpoints: {
+    //   0: {
+    //     pagination: {
+    //       el: ".js-home-best-slider-pagination",
+    //       clickable: true,
+    //     },
+    //     slidesPerView: 1,
+    //   },
+    //   576: {
+    //     pagination: {
+    //       el: ".js-home-best-slider-pagination",
+    //       clickable: true,
+    //     },
+    //     slidesPerView: 2,
+    //   },
+    //   768: {
+    //     pagination: {
+    //       el: ".js-home-best-slider-pagination",
+    //       clickable: true,
+    //     },
+    //     slidesPerView: 3,
+    //
+    //   },
+    //   1290: {
+    //     slidesPerView: 4,
+    //     navigation: {
+    //       nextEl: ".js-home-best-slider-next",
+    //       prevEl: ".js-home-best-slider-prev",
+    //     },
+    //   },
+    //   1560: {
+    //     slidesPerView: 4,
+    //     navigation: {
+    //       nextEl: ".js-home-best-slider-next",
+    //       prevEl: ".js-home-best-slider-prev",
+    //     },
+    //   },
+    // }
+  });
+  const homeNewsSlider = new Swiper(".js-news-slider", {
+    speed: 700,
+    slidesPerView: 3,
+    watchSlidesProgress: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
