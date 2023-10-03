4efe506a1c602903b69954257e266fd10ba6cb84
evgsbel <evgsbel@gmail.com>
2023-10-04 01:47:05 +0400

    start card page

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -144,6 +144,28 @@ $(function () {
     e.preventDefault();
     $(this).toggleClass('is-active');
   });
+  $('.js-card-favorite').on('click', function () {
+    if ($(this).hasClass('item-favorites__actions-button')) {
+      $(this).parents('.item-actions__button-wrapper').toggleClass('is-active');
+    }
+    if ($(this).find('span').text() != "В избранном") {
+      $(this).find('span').text('В избранном');
+    } else {
+      $(this).find('span').text('Избранное');
+    }
+    $(this).toggleClass('is-active');
+  });
+  $('.js-card-matches').on('click', function () {
+    if ($(this).hasClass('item-favorites__actions-button')) {
+      $(this).parents('.item-actions__button-wrapper').toggleClass('is-active');
+    }
+    if ($(this).find('span').text() != "В сравнении") {
+      $(this).find('span').text('В сравнении');
+    } else {
+      $(this).find('span').text('Сравнить');
+    }
+    $(this).toggleClass('is-active');
+  });
 });
 
 //sliders
@@ -305,6 +327,24 @@ $(function () {
       }
     }
   });
+
+  //card slider
+  var pdoductImagesSmall = new Swiper(".js-product-images-slider-small", {
+    spaceBetween: 15,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true
+  });
+  var pdoductImagesBig = new Swiper(".js-product-images-slider-big", {
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".product-images-nav-next",
+      prevEl: ".product-images-nav-prev"
+    },
+    thumbs: {
+      swiper: pdoductImagesSmall
+    }
+  });
 });
 
 //change city
@@ -410,7 +450,6 @@ $(function () {
       var cat = document.createElement("span");
       var div = document.createElement("div");
       var del = document.createElement("span");
-      console.log(cat);
       div.innerText = e.value;
       div.classList.add("catalog-tag-list__item");
       del.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M16.7076 8.70708L13.4146 12.0001L16.7076 15.2931C16.8031 15.3853 16.8793 15.4957 16.9317 15.6177C16.9841 15.7397 17.0117 15.8709 17.0128 16.0037C17.014 16.1365 16.9887 16.2681 16.9384 16.391C16.8881 16.5139 16.8139 16.6256 16.72 16.7195C16.6261 16.8134 16.5144 16.8876 16.3915 16.9379C16.2686 16.9882 16.1369 17.0135 16.0042 17.0123C15.8714 17.0112 15.7402 16.9836 15.6182 16.9312C15.4962 16.8788 15.3858 16.8026 15.2936 16.7071L12.0006 13.4141L8.70757 16.7071C8.61532 16.8026 8.50498 16.8788 8.38297 16.9312C8.26097 16.9836 8.12975 17.0112 7.99697 17.0123C7.86419 17.0135 7.73251 16.9882 7.60962 16.9379C7.48672 16.8876 7.37507 16.8134 7.28117 16.7195C7.18728 16.6256 7.11303 16.5139 7.06275 16.391C7.01247 16.2681 6.98717 16.1365 6.98832 16.0037C6.98947 15.8709 7.01706 15.7397 7.06947 15.6177C7.12188 15.4957 7.19806 15.3853 7.29357 15.2931L10.5866 12.0001L7.29357 8.70708C7.19806 8.61483 7.12188 8.50449 7.06947 8.38249C7.01706 8.26048 6.98947 8.12926 6.98832 7.99648C6.98717 7.8637 7.01247 7.73202 7.06275 7.60913C7.11303 7.48623 7.18728 7.37458 7.28117 7.28069C7.37507 7.18679 7.48672 7.11254 7.60962 7.06226C7.73251 7.01198 7.86419 6.98668 7.99697 6.98783C8.12975 6.98898 8.26097 7.01657 8.38297 7.06898C8.50498 7.12139 8.61532 7.19757 8.70757 7.29308L12.0006 10.5861L15.2936 7.29308C15.3858 7.19757 15.4962 7.12139 15.6182 7.06898C15.7402 7.01657 15.8714 6.98898 16.0042 6.98783C16.1369 6.98668 16.2686 7.01198 16.3915 7.06226C16.5144 7.11254 16.6261 7.18679 16.72 7.28069C16.8139 7.37458 16.8881 7.48623 16.9384 7.60913C16.9887 7.73202 17.014 7.8637 17.0128 7.99648C17.0117 8.12926 16.9841 8.26048 16.9317 8.38249C16.8793 8.50449 16.8031 8.61483 16.7076 8.70708Z" /></svg>';
@@ -437,131 +476,6 @@ $(function () {
   function changeTag() {
     this.checked ? checked.append(this.target) : hidden.append(this.target);
   }
-
-  //shorthand for $(document).ready(handler)
-  //   $(function() {
-  //
-  //     var $groupContainers  = $('.group-containers'),
-  //
-  //       // out 'model': a list of objects representing user choices
-  //       userChoices       = [];
-  //
-  //     $('.filters-check__input').on('change', function(e) {
-  //       let span = document.createElement("span");
-  //       var $checkedBox     = $(this),
-  //         checkedBoxLabel = $checkedBox.next('label').text(),
-  //         checkedValue    = $checkedBox.val(),
-  //         category        = $checkedBox.data('category'),
-  //         alreadyChecked  = !$checkedBox[0].checked;
-  //
-  //       span.innerText = 'del';
-  //       span.onclick = function() {
-  //         e.checked = false;
-  //
-  //       };
-  //       console.log(e)
-  //
-  //
-  //       if(alreadyChecked){
-  //         removeChoiceFromModel(category, checkedValue);
-  //       } else {
-  //         addChoiceToModel(category, checkedValue);
-  //       }
-  //
-  //       updateDom();
-  //
-  //       return;
-  //       /////////////////////////////////////////////////////////
-  //
-  //       function cleanGroupContainers(){
-  //         $groupContainers.each(function(){
-  //           var list = $(this).find('ul');
-  //
-  //           if(list.length){
-  //             list.remove();
-  //           }
-  //         });
-  //       }
-  //
-  //
-  //       function updateDom(){
-  //
-  //         // reset dom - not efficient but easier to
-  //         // keep list state in sync
-  //         cleanGroupContainers();
-  //         appendChoices();
-  //         hideEmptyLists();
-  //       }
-  //
-  //       function appendChoices(){
-  //
-  //         var i, option, $categoryDiv,
-  //
-  //           len = userChoices.length;
-  //
-  //         // if len === 0 then no need to check
-  //         if(!len) return false;
-  //
-  //         for(i = 0; i < len; i++){
-  //           option        = userChoices[i];
-  //           $categoryDiv  = $('#' + option.category + '-container');
-  //
-  //           $categoryDiv.append('<ul><li>'+option.value+'</li></ul>');
-  //
-  //
-  //
-  //         }
-  //
-  //       }
-  //
-  //       function hideEmptyLists(){
-  //         $groupContainers.each(function(){
-  //           var $container = $(this),
-  //             items = $container.find('ul > li');
-  //
-  //           if(!items.length){
-  //             $container.hide();
-  //             // skip to next iteration
-  //             return true;
-  //           }
-  //
-  //           $container.show();
-  //         });
-  //       }
-  //
-  //       function addChoiceToModel(category, value){
-  //         userChoices.push({
-  //           category: category,
-  //           value: checkedValue
-  //         });
-  //       }
-  //
-  //       function removeChoiceFromModel(category, value){
-  //
-  //         var i, option,
-  //
-  //           len = userChoices.length;
-  //
-  //         // if len === 0 then no need to check
-  //         if(!len) return false;
-  //
-  //         for(i = 0; i < len; i++){
-  //           option = userChoices[i];
-  //
-  //           if(option.category === category && option.value === value){
-  //             // remove choice object from array
-  //             userChoices.splice(i, 1);
-  //             return;
-  //           }
-  //         }
-  //
-  //       }
-  //
-  //     });
-  //
-  //
-  //
-  //   });
 });
 
 //filters show more
@@ -595,6 +509,8 @@ $(document).ready(function () {
     $('.catalog').addClass('catalog_horizontal');
     $('.catalog').removeClass('catalog_vertical');
     $(this).addClass('is-active');
+    $(this).attr('disabled', '');
+    $(".js-vertical-view").removeAttr('disabled');
     $('.js-vertical-view').removeClass('is-active');
     if ($('.js-catalog-item').hasClass('col-12')) {
       $('.js-catalog-item').addClass('col-lg-4 col-sm-6');
@@ -609,6 +525,8 @@ $(document).ready(function () {
     $('.catalog').removeClass('catalog_horizontal');
     $('.catalog').addClass('catalog_vertical');
     $(this).addClass('is-active');
+    $(this).attr('disabled', '');
+    $(".js-horizontal-view").removeAttr('disabled');
     $('.js-horizontal-view').removeClass('is-active');
     if ($('.js-catalog-item').hasClass('col-lg-4 col-sm-6')) {
       $('.js-catalog-item').addClass('col-12');
